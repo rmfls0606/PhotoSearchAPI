56d7207fcf7d9026b23008785ba6f89b6fac924d
rmfls0606 <rmfls0606@naver.com>
2025-01-22 06:30:29 +0900

    디테일뷰 정보 표시

--- PhotoSearchAPI/DetailViewController.swift
+++ PhotoSearchAPI/DetailViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Kingfisher
+import SnapKit
 
 struct StatisticsResponse: Decodable{
     let downloads: StatisticsDownload
@@ -22,7 +23,7 @@ struct StatisticsView: Decodable{
 }
 
 class DetailViewController: UIViewController {
-
+    
     var item: SearchResult?
     private var statisticsData: StatisticsResponse? = nil
     
@@ -31,6 +32,63 @@ class DetailViewController: UIViewController {
         view.contentMode = .scaleAspectFill
         return view
     }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "정보"
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var sizeLabel = createLabel(text: "크기")
+    private lazy var viewsLabel = createLabel(text: "조회수")
+    private lazy var downloadsLabel = createLabel(text: "다운로드")
+    
+    private lazy var sizeValueLabel = createLabel(text: "\(item!.width) x \(item!.height)")
+    private lazy var viewsValueLabel = createLabel(text: "")
+    private lazy var downloadsValueLabel = createLabel(text: "")
+    
+    private lazy var keyStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [sizeLabel, viewsLabel, downloadsLabel])
+        view.axis = .vertical
+        view.spacing = 8
+        view.alignment = .leading
+        return view
+    }()
+    
+    private lazy var valueStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [sizeValueLabel, viewsValueLabel, downloadsValueLabel])
+        view.axis = .vertical
+        view.spacing = 8
+        view.alignment = .trailing
+        return view
+    }()
+    
+    private lazy var mainStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [keyStackView, valueStackView])
+        view.axis = .horizontal
+        view.spacing = 16
+        return view
+    }()
+    
+    private lazy var boxView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [titleLabel, mainStackView])
+        view.spacing = 30
+        view.axis = .horizontal
+        return view
+    }()
+    
+//    private lazy var scrollView: UIScrollView = {
+//        let view = UIScrollView(frame: .zero)
+//        view.addSubview(imageView)
+//        view.addSubview(titleLabel)
+//        view.addSubview(mainStackView)
+//        return view
+//    }()
+    
+//    private lazy var outerview: UIStackView = {
+//        let view = UIStackView(arrangedSubviews: [imageView, titleLabel,])
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,13 +99,42 @@ class DetailViewController: UIViewController {
     
     private func setUp(){
         self.view.backgroundColor = .white
+        self.view.addSubview(boxView)
+//        self.view.addSubview(scrollView)
         self.view.addSubview(imageView)
+//        self.view.addSubview(titleLabel)
+//        view.addSubview(mainStackView)
+        self.sizeLabel.font = .boldSystemFont(ofSize: 14)
+        self.viewsLabel.font = .boldSystemFont(ofSize: 14)
+        self.downloadsLabel.font = .boldSystemFont(ofSize: 14)
+        
+        boxView.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(12)
+            make.trailing.equalToSuperview().offset(-12)
+            make.leading.equalToSuperview().offset(12)
+        }
+//        scrollView.snp.makeConstraints { make in
+//            make.top.equalTo(self.view.safeAreaLayoutGuide)
+//            make.leading.bottom.trailing.equalToSuperview()
+//        }
         
         imageView.kf.setImage(with: URL(string: item!.urls.small))
         
         imageView.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.centerX.equalToSuperview()
+//            make.height.equalTo(1000)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(boxView)
+        }
+        
+        mainStackView.snp.makeConstraints { make in
+            make.top.equalTo(boxView)
+            make.leading.equalTo(titleLabel.snp.trailing).offset(30)
+            make.trailing.equalTo(boxView)
+            
         }
     }
     
@@ -65,10 +152,18 @@ class DetailViewController: UIViewController {
             switch result {
             case .success(let success):
                 self.statisticsData = success
-                print(success)
+                self.viewsValueLabel.text = success.views.total.formatted(.number)
+                self.downloadsValueLabel.text = success.downloads.total.formatted(.number)
             case .failure(let failure):
                 fatalError(failure.localizedDescription)
             }
         })
+    }
+    
+    private func createLabel(text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
     }
 }
--- PhotoSearchAPI/SearchResultCollectionView.swift
+++ PhotoSearchAPI/SearchResultCollectionView.swift
@@ -64,7 +64,7 @@ final class SearchResultCollectionView: BaseCollectionViewCell{
     
     func configureData(data: SearchResult){
         let liksetText = data.likes.formatted(.number)
-        self.itemImageView.kf.setImage(with: URL(string: data.urls.thumb))
+        self.itemImageView.kf.setImage(with: URL(string: data.urls.small))
         self.starButton.setTitle(liksetText, for: .normal)
         self.starButton.setTitle(liksetText, for: .highlighted)
     }
